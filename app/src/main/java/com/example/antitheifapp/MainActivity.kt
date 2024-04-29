221037877d68b3b1998247267220e27db817e747
ashfaqabasyn <ashfaq.abasyn@gmail.com>
2024-04-29 14:13:47 +0500

    finished project

--- app/src/main/java/com/example/antitheifapp/MainActivity.kt
+++ app/src/main/java/com/example/antitheifapp/MainActivity.kt
@@ -7,6 +7,11 @@ import com.example.antitheifapp.databinding.ActivityMainBinding
 import com.example.antitheifapp.services.ThiefService
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.os.Build
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,11 +21,15 @@ class MainActivity : AppCompatActivity() {
     private var isMotionEnabled = false
     private var isChargerEnabled = false
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),0)
+        }
 
         sharedPreferences = getSharedPreferences("DetectionPrefs", Context.MODE_PRIVATE)
         // Retrieve saved values from SharedPreferences
@@ -30,32 +39,58 @@ class MainActivity : AppCompatActivity() {
 
         // Update UI based on saved values
         updateUI()
-
-        binding.btnChargerRemoval.setOnClickListener {
-            if (isChargerEnabled) {
-                disableChargerRemoval()
-            } else {
-                enableChargerRemoval()
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            binding.btnChargerRemoval.setOnClickListener {
+                if (isChargerEnabled) {
+                    disableChargerRemoval()
+                } else {
+                    enableChargerRemoval()
+                }
             }
+        } else {
+            requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 0)
         }
 
-        binding.btnMotionDecter.setOnClickListener {
-            if (isMotionEnabled) {
-                disableMotionDetection()
-            } else {
-                enableMotionDetection()
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            binding.btnMotionDecter.setOnClickListener {
+                if (isMotionEnabled) {
+                    disableMotionDetection()
+                } else {
+                    enableMotionDetection()
+                }
             }
+        } else {
+            requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 0)
         }
 
-        binding.btnPocketRemoval.setOnClickListener {
-            if (isPocketEnabled) {
-                disablePocketDetection()
-            } else {
-                enablePocketDetection()
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            binding.btnPocketRemoval.setOnClickListener {
+                if (isPocketEnabled) {
+                    disablePocketDetection()
+                } else {
+                    enablePocketDetection()
+                }
             }
+        } else {
+            requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 0)
         }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==0){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this@MainActivity, "Granted", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this@MainActivity, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
     private fun enablePocketDetection() {
         val intent = Intent(this, ThiefService::class.java)
         intent.action = ThiefService.ACTION_POCKET_REMOVED
@@ -131,7 +166,6 @@ class MainActivity : AppCompatActivity() {
         editor.putBoolean("isChargerEnabled", isChargerEnabled)
         editor.apply()
     }
-
     private fun stopServiceIfAllDisabled() {
         if (!isPocketEnabled && !isMotionEnabled && !isChargerEnabled) {
             val serviceIntent = Intent(this, ThiefService::class.java)
